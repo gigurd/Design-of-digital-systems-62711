93228633eaa67ba31000c8df1f881bcb34dad3a9
AndreasJacobsen <s241123@dtu.dk>
2026-02-13 11:36:03 +0100

    First Commit
    lort

---
+++ PWA/PWA.srcs/sources_1/new/TOP_MODUL.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 13.02.2026 10:52:10
+-- Design Name: 
+-- Module Name: TOP_MODUL - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TOP_MODUL is
+    Port ( vara : in STD_LOGIC;
+           varb : in STD_LOGIC);
+end TOP_MODUL;
+
+architecture Behavioral of TOP_MODUL is
+
+begin
+
+
+end Behavioral;

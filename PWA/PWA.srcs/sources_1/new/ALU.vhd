04ab3aa8c014b217780825041567b6cb202bfb0e
Jonas Beck Jensen <jonasbjensen@outlook.dk>
2026-02-27 15:42:47 +0100

    IDK

--- PWA/PWA.srcs/sources_1/new/ALU.vhd
+++ PWA/PWA.srcs/sources_1/new/ALU.vhd
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------------
 -- Module Name: ALU
 -- Description: Arithmetic Logic Unit (PWA spec)
---              J_Select(3:0) controls the operation:
+--              JSel(3:0) controls the operation:
 --                J3=0: arithmetic (J2=S1, J1=S0, J0=Cin)
 --                  0000: F=A       0001: F=A+1
 --                  0010: F=A+B     0011: F=A+B+1
@@ -21,7 +21,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ALU is
     Port (
         A, B     : in  STD_LOGIC_VECTOR(7 downto 0);
-        J_Select : in  STD_LOGIC_VECTOR(3 downto 0);
+        JSel     : in  STD_LOGIC_VECTOR(3 downto 0);
         Cin      : in  STD_LOGIC;
         V, C     : out STD_LOGIC;
         J        : out STD_LOGIC_VECTOR(7 downto 0)
@@ -30,9 +30,19 @@ end ALU;
 
 architecture ALU_Structural of ALU is
 
-
 signal ASig, BSig: STD_LOGIC_VECTOR(7 downto 0);
 begin
+    
+    Asig <= A;
+    Bsig(0) <= (B(0) AND JSel(0)) OR ((NOT B(0)) AND JSel(1));
+    Bsig(1) <= (B(1) AND JSel(0)) OR ((NOT B(1)) AND JSel(1));
+    Bsig(2) <= (B(2) AND JSel(0)) OR ((NOT B(2)) AND JSel(1));
+    Bsig(3) <= (B(3) AND JSel(0)) OR ((NOT B(3)) AND JSel(1));
+    Bsig(4) <= (B(4) AND JSel(0)) OR ((NOT B(4)) AND JSel(1));
+    Bsig(5) <= (B(5) AND JSel(0)) OR ((NOT B(5)) AND JSel(1));
+    Bsig(6) <= (B(6) AND JSel(0)) OR ((NOT B(6)) AND JSel(1));
+    Bsig(7) <= (B(7) AND JSel(0)) OR ((NOT B(7)) AND JSel(1));
+
 
     full_adder: entity work.full_adder_8_bit
     port map(
@@ -42,10 +52,5 @@ begin
             Cin  => Cin,
             Cout =>  C
     );
-
-
-
-
-
 
 end ALU_Structural;
--- PWA/PWA.srcs/sources_1/new/full_adder_8_bit.vhd
+++ PWA/PWA.srcs/sources_1/new/full_adder_8_bit.vhd
@@ -5,7 +5,7 @@ entity full_adder_8_bit is
     Port ( A    : in STD_LOGIC_VECTOR (7 downto 0);
            B    : in STD_LOGIC_VECTOR (7 downto 0);
            sum  : out STD_LOGIC_VECTOR (7 downto 0);
-           Cin  : in STD_LOGIC_VECTOR (7 downto 0);
+           Cin  : in STD_LOGIC;
            Cout : out STD_LOGIC
            );
 end full_adder_8_bit;
